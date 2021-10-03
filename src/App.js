540c5b93effe4cea586acdc171f1093b7d88a6de
trekvartista <isken_150502@mail.ru>
2021-10-04 02:14:29 +0600

    Turn a button into a component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import './components/sidebar'
-import Sidebar from './components/sidebar';
+import './components/Sidebar/Sidebar'
+import Sidebar from './components/Sidebar/Sidebar';
+import OpenButton from './components/OpenButton/OpenButton'
 
 function App() {
 
-    function openNav() {
-        document.getElementById("mySidebar").style.width = "250px";
-    }
-
     return (
         <div>
-            <h1 style={{marginLeft: 100, color: '#555'}}> a Very useful website... </h1>
-            <p>Click on the element below to open the side navigation menu.</p>
+            <h1> a Very useful website... </h1>
+            <p> Click on the element below to open the side navigation menu. </p>
             <Sidebar />
-            <span className="openbtn" onClick={openNav}> &#9776; Open </span>
+            <OpenButton />
+
         </div>
     );
 }
