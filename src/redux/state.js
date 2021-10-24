930936323922aa88654e139a00b756216b14fd26
Kutmanbekov Iskender <isken_150502@mail.ru>
2021-10-25 01:14:34 +0600

    introducing redux

---
+++ src/redux/state.js
@@ -0,0 +1,12 @@
+let state = {
+    profilePage: {
+        posts: [],
+        dialogs: []
+    },
+    messagesPage: {
+        messages: [
+            {id: 1, message: "Hi, I'm here!"},
+            {id: 2, message: "It's my second message."}
+        ]
+    }
+}
