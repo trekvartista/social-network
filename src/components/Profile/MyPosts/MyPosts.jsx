cecfeffb1825197e35184b6f2e1f50b1f90c223a
Kutmanbekov Iskender <isken_150502@mail.ru>
2021-10-26 02:13:21 +0600

    create Post and MyPost Components

---
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Post from '../Post/Post';
+import s from './MyPosts.module.css'
+
+function MyPosts(props)  {
+
+    let postElements = props.state.profilePage.posts.map((p) => (
+        <Post message={p.text} likes={p.likesCount} />
+    ));
+
+    let newPost = React.createRef();
+
+    let addPost = () => {
+        let text = newPost.current.value;
+        alert(text);
+        newPost.current.value = "";
+    }
+    /* alert('You do want it.'); */
+    return (
+        <div className={s.main}>
+            <h2>My Posts</h2>
+            <div>
+                <textarea ref={newPost}></textarea>
+            </div>
+
+            <div>
+                <button onClick={addPost}>Add post</button>
+            </div>
+            <div>
+                {postElements}
+            </div>
+        </div>
+    );
+}
+
+export default MyPosts;
---
+++ src/components/Profile/Post/Post.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ava from "../../../images/avatarka.png";
+function Post(props) {
+    return(
+
+        <div>
+            <img    
+                src={ava}
+                width="50px"
+                height="50px"
+                alt=""/>
+            {props.message}
+            <div>
+                <span> {props.likes} Like(s) </span>
+            </div>
+        </div>
+    );
+}
+
+export default Post;
