d3f286bb837c40ab378539e4661bb1924a45b9e2
Kutmanbekov Iskender <isken_150502@mail.ru>
2021-12-25 01:08:17 +0600

    info about the hooks

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -6,6 +6,6 @@ let reducers = combineReducers({
     profilePage: profileReducer     // diff names
 });
 
-let store = createStore(reducers);
+let store = createStore(reducers);  // should I use it in this project? useReducer, useContext and other hooks could probably give the same results
 
 export default store;
