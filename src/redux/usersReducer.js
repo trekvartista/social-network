f8cebad01bd5aad3cb721c7cf9c4202265bf53ef
Kutmanbekov Iskender <isken_150502@mail.ru>
2021-12-30 23:46:26 +0600

    initialize users state

---
+++ src/redux/usersReducer.js
@@ -0,0 +1,28 @@
+
+
+let initialState = {
+    users: [
+        {
+            id: 1,
+            firstName: 'Ricardo',
+            lastName: 'Milos',
+            status: 'online',
+            location: {
+                country: 'Memeland',
+                city: 'Memass'
+            }
+        },
+        {
+            id: 2,
+            firstName: 'Sadyr',
+            lastName: 'Japarov',
+            status: 'online',
+            location: {
+                country: 'Kyrgyzstan',
+                city: 'Bishkek'
+            }
+        },
+        {}
+    ]
+
+}
