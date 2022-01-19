ac21453bfef6ff3ea39ec178c8ecb6d855d5c42e
Kutmanbekov Iskender <isken_150502@mail.ru>
2022-01-20 01:23:41 +0600

    set up api environment (working with the instance of axios)

--- src/api/api.js
+++ src/api/api.js
@@ -1,62 +1,50 @@
 import * as axios from 'axios';
 
+const axiosInstance = axios.create({
+
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    withCredentials: true,
+    headers: {
+        "API-KEY": 'efce9c83-1ae0-4516-8a1f-403dddee3b1f'
+    }
+})
+
 export const getUsers = (currentPage = 1, pageSize = 100) => {
     return (
-        axios
-            .get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`,
-            {
-                withCredentials: true
-            })
+        axiosInstance
+            .get(`users?page=${currentPage}&count=${pageSize}`)
             .then(response => response.data)
     )
 }
 
 export const getUserProfile = (userId) => {
     return (
-        axios
-            .get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`,
-            {
-                withCredentials: true
-            })
+        axiosInstance
+            .get(`profile/${userId}`)
             .then(response => response.data)
     )
 }
 
 export const authMe = () => {
     return (
-        axios
-            .get(`https://social-network.samuraijs.com/api/1.0/auth/me`,
-            {
-                withCredentials: true
-            })
+        axiosInstance
+            .get(`auth/me`)
             .then(response => response.data)
     )
 }
 
 export const follow = (userId) => {
     return (
-        axios
-            .post(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`, {},
-            {
-                withCredentials: true,
-                headers: {
-                    "API-KEY": 'efce9c83-1ae0-4516-8a1f-403dddee3b1f'
-                }
-            })
+        axiosInstance
+            .post(`follow/${userId}`, {})
             .then(response => response.data)
     )
 }
 
 export const unfollow = (userId) => {
     return (
-        axios
-        .delete(`https://social-network.samuraijs.com/api/1.0/follow/${userId}`,
-            {
-                withCredentials: true,
-                headers: {
-                    "API-KEY": 'efce9c83-1ae0-4516-8a1f-403dddee3b1f'
-                }
-            })
+        axiosInstance
+            .delete(`follow/${userId}`)
             .then(response => response.data)
     )
 }
