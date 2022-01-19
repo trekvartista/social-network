36b15fd292793366d8381a4326d12f65b16df299
Kutmanbekov Iskender <isken_150502@mail.ru>
2022-01-19 22:50:51 +0600

    isAuthorized property

--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -3,13 +3,14 @@ const AUTH_USER = 'AUTH_USER';
 let initialState = {
     userId: null,
     email: null,
-    login: null
+    login: null,
+    isAuthorized: false
 }
 
 const authReducer = (state = initialState, action) => {
 
     switch(action.type) {
-        case AUTH_USER: return {...state, ...action.data}
+        case AUTH_USER: return {...state, ...action.data, isAuthorized: true}
         default: return state
     }
 }
