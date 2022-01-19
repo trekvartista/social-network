6695b244e86f1a9744b40fa3eef0271660de751c
Kutmanbekov Iskender <isken_150502@mail.ru>
2022-01-19 22:34:02 +0600

    add new auth Reducer to the store

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,11 +1,13 @@
 import { combineReducers, createStore } from "redux";
+import authReducer from "./authReducer";
 import profileReducer from './profileReducer'
 import usersReducer from "./usersReducer";
 
 // all reducers come here !
 let reducers = combineReducers({
     profilePage: profileReducer,     // diff names
-    usersPage: usersReducer
+    usersPage: usersReducer,
+    auth: authReducer
 });
 
 let store = createStore(reducers);  // should I use it in this project? useReducer, useContext and other hooks could probably give the same results
