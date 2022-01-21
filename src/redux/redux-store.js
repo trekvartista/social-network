378f8286a1fd38632f380999a8a4235fc82af33c
Kutmanbekov Iskender <isken_150502@mail.ru>
2022-01-22 01:40:39 +0600

    apply Thunk Middleware as a parameter in 'createStore'

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,7 +1,8 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
 import authReducer from "./authReducer";
 import profileReducer from './profileReducer'
 import usersReducer from "./usersReducer";
+import thunkMiddleware from 'redux-thunk';
 
 // all reducers come here !
 let reducers = combineReducers({
@@ -10,7 +11,7 @@ let reducers = combineReducers({
     auth: authReducer
 });
 
-let store = createStore(reducers);  // should I use it in this project? useReducer, useContext and other hooks could probably give the same results
+let store = createStore(reducers, applyMiddleware(thunkMiddleware));  // should I use it in this project? useReducer, useContext and other hooks could probably give the same results
 
 // window.store = store
 
