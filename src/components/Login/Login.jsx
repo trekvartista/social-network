88392362ec1552b53ebf774b66caceb48ecca42e
Kutmanbekov Iskender <isken_150502@mail.ru>
2022-01-24 01:36:16 +0600

    empty login page

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Login.module.css';
+
+let Login = (props) => {
+    return (
+        <>
+            <h1 className={s}> Login Page </h1>
+        </>
+    )
+}
+
+export default Login;
