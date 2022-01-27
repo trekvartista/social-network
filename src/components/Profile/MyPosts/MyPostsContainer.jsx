9832b6d287c4e1a4978896bd162d62b2b277b9f9
Kutmanbekov Iskender <isken_150502@mail.ru>
2022-01-28 03:44:46 +0600

    remove warnings

--- src/components/Profile/MyPosts/MyPostsContainer.jsx
+++ src/components/Profile/MyPosts/MyPostsContainer.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { connect } from 'react-redux';
 import { addPostActionCreator, updateNewTextActionCreator } from '../../../redux/profileReducer';
 import MyPosts from './MyPosts';
