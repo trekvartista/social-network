0f1c7a1e28785123fadd7b7cbdde9e91748b9da2
Kutmanbekov Iskender <isken_150502@mail.ru>
2022-02-01 22:53:53 +0600

    sample: React.lazy(), React.Suspense fallback

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import HomePage from './components/HomePage/HomePage';
-import ProfileContainer from './components/Profile/ProfileContainer';
-import Sidebar from './components/Sidebar/Sidebar';
-import Contact from './components/Contact/Contact';
-import Portfolio from './components/Portfolio/Portfolio';
-import Footer from './components/Footer/Footer';
 import { Route } from 'react-router-dom';
-import UsersContainer from './components/Users/UsersContainer';
+import HomePage from './components/HomePage/HomePage';
 import HeaderContainer from './components/Header/HeaderContainer';
-import LoginPage from './components/LoginPage/LoginPage'
+import Sidebar from './components/Sidebar/Sidebar';
+import LoginPage from './components/LoginPage/LoginPage';
+import ProfileContainer from './components/Profile/ProfileContainer';
+import UsersContainer from './components/Users/UsersContainer';
+// import Contact from './components/Contact/Contact';
+// import Portfolio from './components/Portfolio/Portfolio';
+import Footer from './components/Footer/Footer';
+import loading from './images/loading.gif';
+
+const Contact = React.lazy( () => import('./components/Contact/Contact') )
+const Portfolio = React.lazy( () => import('./components/Portfolio/Portfolio') )
 
 function App() {
 
@@ -18,11 +22,17 @@ function App() {
                 <Sidebar />
                 <div>
                     <Route path='/' exact={true} render={() => <HomePage />} />
-                    <Route path='/profile' render={() => <ProfileContainer/>} />
-                    <Route path='/contact' render={() => <Contact />} />
-                    <Route path='/users' render={() => <UsersContainer />}/>
-                    <Route path='/portfolio' render={() => <Portfolio />} />
                     <Route path='/login' render={() => < LoginPage />} />
+                    <Route path='/profile' render={() => <ProfileContainer/>} />
+                    <Route path='/users' render={() => <UsersContainer />}/>
+                    <Route path='/contact' render={() => (
+                                            <React.Suspense fallback={loading}>
+                                                <Contact />
+                                            </React.Suspense>)} />
+                    <Route path='/portfolio' render={() => (
+                                            <React.Suspense fallback={loading}>
+                                                <Portfolio />
+                                            </React.Suspense>)} />
                 </div>
                 <Footer />
             </div>
