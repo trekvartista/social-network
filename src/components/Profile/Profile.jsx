21d43eb40b6ae6aeb9ca752cb8146b5f7e81ffd9
Kutmanbekov Iskender <isken_150502@mail.ru>
2022-02-03 02:13:13 +0600

    Change outlook of profile page

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,16 +1,17 @@
 import s from "./Profile.module.css";
 import loading from "../../images/loading.gif";
+import header from "../../images/header.jpg";
 import defaultUserPhoto from "../../images/defaultUserPhoto.jpg";
 import MyPostsContainer from "./MyPosts/MyPostsContainer";
 import { useEffect } from "react";
 import { useRouteMatch } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 
-function Profile(props) {
+function Profile({ profile, isAuthorized, myUserID, getUserProfile, savePhoto }) {
 
     const uploadPhoto = (e) => {
         if (e.target.files.length) {
-            props.savePhoto(e.target.files[0]);
+            savePhoto(e.target.files[0]);
         }
     }
 
@@ -18,54 +19,80 @@ function Profile(props) {
     let match = useRouteMatch("/profile/:userId?");
     
     useEffect(() => {
-        if (!props.isAuthorized) {
+        if (!isAuthorized) {
             history.push('/login')
         }
         let userId = match.params.userId;
 
         if (!userId) { 
-            userId = props.myUserID
+            userId = myUserID
         }
         if (userId) {
-            props.getUserProfile(userId);
+            getUserProfile(userId);
 
         }
 
         window.scrollTo(0, 0)
 
-    }, [history, match.params.userId, props.myUserID]);
+    }, [history, match.params.userId, myUserID]);
     
-    if (!props.profile) {
+    if (!profile) {
         return <img className={s.loading} src={loading} />;
     }
     
     return (
         <div className={s.main}>
-            <div className={s.header}>
-                <img
-                    id={s.img}
-                    src="https://wallpapers.com/images/high/dual-monitor-vaporwave-city-3vn0dif6tnz55r2x.jpg"
-                    width="1000px"
-                    height="200px"
-                    alt=""
-                />
-            </div>
-            <div className={s.ava}>
-                <img id={s.ava} src={props.profile.photos.small || defaultUserPhoto} alt="" />
+            <div className={s.central}>
+                <div className={s.header}>
+                    <img
+                        id={s.img}
+                        src={header}
+                        alt=""
+                    />
+                </div>
+                <div className={s.ava}>
+                    <img id={s.ava} src={profile.photos.small || defaultUserPhoto} alt="" />
+                    <div className={s.uploadPhoto}>
+                        {!match.params.userId ? <input type="file"
+                            onChange={uploadPhoto}
+                            >
+                        </input> : null}
+                    </div>
+                </div>
+
+                <div className={s.info}>
+                    <p style={{ fontSize: 25 }}> {profile.fullName} </p>
+                    
+                    <b> About me: {profile.aboutMe} </b>
+
+                    <div>
+                        <b> Looking for a job: </b> {profile.lookingForAJob ? "yes" : "no"}
+                    </div>
+                    {
+                        profile.lookingForAJob &&
+                        <div>
+                            <b>My professional skills: </b> {profile.lookingForAJobDescription}
+                        </div>
+                    }
+
+                    <div>
+                        <b>Status: {profile.status}</b>
+                    </div>
+
+                    <div>
+                        <b>Contacts:</b> {Object.keys(profile.contacts).map(key => {
+                            return profile.contacts[key] && <div><b>{key}:</b> {profile.contacts[key]}</div>
+                        })}
+
+                    </div>
+
+                </div>
+
+                <div className={s.posts}>
+                    <MyPostsContainer className={s.child} />
+                </div>
             </div>
 
-            <div className={s.info}>
-                <p style={{ fontSize: 25 }}> {props.profile.fullName} </p>
-                <p> {props.profile.aboutMe} </p>
-                {!match.params.userId ? <input type="file"
-                    onChange={uploadPhoto}
-                    className={s.uploadPhoto}>
-                </input> : null}
-            </div>
-
-            <div className={s.posts}>
-                <MyPostsContainer className={s.child} />
-            </div>
         </div>
     );
 }
