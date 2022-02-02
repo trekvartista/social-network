bf29e480953e7aa8bbcae601034f6ffde82fedbc
Kutmanbekov Iskender <isken_150502@mail.ru>
2022-02-03 03:29:53 +0600

    Profile page: edit profile info sample

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -6,35 +6,37 @@ import MyPostsContainer from "./MyPosts/MyPostsContainer";
 import { useEffect } from "react";
 import { useRouteMatch } from "react-router-dom";
 import { useHistory } from "react-router-dom";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 function Profile({ profile, isAuthorized, myUserID, getUserProfile, savePhoto }) {
+
+    const [editMode, setEditMode] = useState(false)
+    const history = useHistory();
+    let match = useRouteMatch("/profile/:userId?");
+    let userId = match.params.userId;
+    
+    useEffect(() => {
+        if (!isAuthorized) {
+            history.push('/login')
+        }
+
+        if (!userId) { 
+            userId = myUserID;
+        }
+        if (userId) {
+            getUserProfile(userId);
+        }
+
+        window.scrollTo(0, 0)
+
+    }, [history, match.params.userId, myUserID]);
 
     const uploadPhoto = (e) => {
         if (e.target.files.length) {
             savePhoto(e.target.files[0]);
         }
     }
-
-    const history = useHistory();
-    let match = useRouteMatch("/profile/:userId?");
-    
-    useEffect(() => {
-        if (!isAuthorized) {
-            history.push('/login')
-        }
-        let userId = match.params.userId;
-
-        if (!userId) { 
-            userId = myUserID
-        }
-        if (userId) {
-            getUserProfile(userId);
-
-        }
-
-        window.scrollTo(0, 0)
-
-    }, [history, match.params.userId, myUserID]);
     
     if (!profile) {
         return <img className={s.loading} src={loading} />;
@@ -53,40 +55,16 @@ function Profile({ profile, isAuthorized, myUserID, getUserProfile, savePhoto })
                 <div className={s.ava}>
                     <img id={s.ava} src={profile.photos.small || defaultUserPhoto} alt="" />
                     <div className={s.uploadPhoto}>
-                        {!match.params.userId ? <input type="file"
+                        {!userId ? <input type="file"
                             onChange={uploadPhoto}
                             >
                         </input> : null}
                     </div>
                 </div>
-
-                <div className={s.info}>
-                    <p style={{ fontSize: 25 }}> {profile.fullName} </p>
-                    
-                    <b> About me: {profile.aboutMe} </b>
-
-                    <div>
-                        <b> Looking for a job: </b> {profile.lookingForAJob ? "yes" : "no"}
-                    </div>
-                    {
-                        profile.lookingForAJob &&
-                        <div>
-                            <b>My professional skills: </b> {profile.lookingForAJobDescription}
-                        </div>
-                    }
-
-                    <div>
-                        <b>Status: {profile.status}</b>
-                    </div>
-
-                    <div>
-                        <b>Contacts:</b> {Object.keys(profile.contacts).map(key => {
-                            return profile.contacts[key] && <div><b>{key}:</b> {profile.contacts[key]}</div>
-                        })}
-
-                    </div>
-
-                </div>
+                { editMode
+                        ? <ProfileInfoForm profile={profile}/>
+                        : <ProfileInfo profile={profile} isOwner={!userId} editModeOn={() => {setEditMode(true)}}/>
+                }
 
                 <div className={s.posts}>
                     <MyPostsContainer className={s.child} />
@@ -95,6 +73,113 @@ function Profile({ profile, isAuthorized, myUserID, getUserProfile, savePhoto })
 
         </div>
     );
+}
+
+const ProfileInfo = ({ profile, isOwner, editModeOn }) => {
+
+    return (
+        <div className={s.info}>
+
+            { isOwner &&
+                <div>
+                    <button onClick={editModeOn}>
+                        Edit
+                    </button>
+                </div>
+            }
+
+            <b className={s.name}> Full name: {profile.fullName} </b>
+            
+            <div className={s.field}>    
+                <b> About me: </b> {profile.aboutMe}
+            </div>
+
+            <div className={s.field}>
+                <b> Looking for a job: </b> {profile.lookingForAJob ? "yes" : "no"}
+            </div>
+            {
+                profile.lookingForAJob &&
+                <div className={s.field}>
+                    <b>My professional skills: </b> {profile.lookingForAJobDescription}
+                </div>
+            }
+
+            <div className={s.field}>
+                <b>Status: {profile.status}</b>
+            </div>
+
+            <div className={s.contacts}>
+                <b>Contacts:</b> {Object.keys(profile.contacts).map(key => {
+                    return profile.contacts[key] && <div><b>{key}:</b> {profile.contacts[key]}</div>
+                })}
+
+            </div>
+
+        </div>
+    )
+}
+
+const ProfileInfoForm = ({ profile }) => {
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    return (
+        <div className={s.info}>
+                <div>
+                    <button>
+                        Save
+                    </button>
+                </div>
+            <form onSubmit={handleSubmit(data => {console.log(data)})}>
+                <div className={s.name}>
+                    <b> Full name: </b>
+                    <input className={s.input}
+                        {...register("name")}
+                        placeholder="Full name"
+                    />
+                </div>
+
+                <div className={s.field}>
+                    <b> About me: </b>
+                    <input className={s.input}
+                        {...register("aboutMe")}
+                        placeholder="About me:"
+                    />
+                </div>
+
+                <div className={s.field}>
+                    <b>Looking for a job:</b>
+                    <input className={s.input}
+                        {...register("lookingForAJob")}
+                        type="checkbox"
+                        style={{marginLeft: "-100px"}}
+                    />
+                </div>
+
+                <div className={s.field}>
+                    <b>Status:</b>
+                    <input className={s.input}
+                        {...register("status")}
+                        placeholder="Status"
+                    />
+                </div>
+            </form>
+            {
+                profile.lookingForAJob &&
+                <div>
+                    <b>My professional skills: </b> {profile.lookingForAJobDescription}
+                </div>
+            }
+
+            <div className={s.contacts}>
+                <b>Contacts:</b> {Object.keys(profile.contacts).map(key => {
+                    return profile.contacts[key] && <div><b>{key}:</b> {profile.contacts[key]}</div>
+                })}
+
+            </div>
+
+        </div>
+    )
 }
 
 export default Profile;
